1ae264fabe9364d03102581be598d70ade2ce98b
Chris Yammine <chris.yammine@hotmail.com>
2014-05-27 16:01:03 -0700

    Clean user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
+  # scope: :full_search, lambda { |search_item| where(["first_name LIKE ? OR last_name LIKE ? OR email LIKE ?", "%#{search_term}%", "%#{search_term}%", "%#{search_term}%"]) }
+  # scope: :date_range, -> { |date1, date2| where(["created_at > ?", date1]).where("created_at < ?", date2) }
 
   def self.date_range(date1, date2)
     where(["created_at > ?", date1]).where("created_at < ?", date2) 
     ### Example date1 = 3.days.ago & date2 = 2.days.ago
+    ### date1 = 30.minutes.ago & date2 = 10.minutes.ago
   end
 
   def self.full_search(search_term)
-    where(["first_name LIKE ? OR last_name LIKE ? OR email LIKE ?", "%#{search_term}%", "%#{search_term}%", "%#{search_term}%"])
+    where(["first_name LIKE ? OR last_name LIKE ? OR email LIKE ?", 
+      "%#{search_term}%", "%#{search_term}%", "%#{search_term}%"])
   end
 
 end
