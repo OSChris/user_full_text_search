06a554c30648c1ce05018c29eb7b776bac22e83c
Chris Yammine <chris.yammine@hotmail.com>
2014-05-27 15:56:50 -0700

    Add date_range method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
 
+  def self.date_range(date1, date2)
+    where(["created_at > ?", date1]).where("created_at < ?", date2) 
+    ### Example date1 = 3.days.ago & date2 = 2.days.ago
+  end
+
   def self.full_search(search_term)
     where(["first_name LIKE ? OR last_name LIKE ? OR email LIKE ?", "%#{search_term}%", "%#{search_term}%", "%#{search_term}%"])
   end
